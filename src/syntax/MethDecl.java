da8980dfb9e5733e56c60dd8379d59b41ffdce2c
Jim Witschey <jim.witschey@gmail.com>
2014-10-30 19:02:20 -0400

    adds information to method declaration check

--- src/syntax/MethDecl.java
+++ src/syntax/MethDecl.java
@@ -61,10 +61,19 @@ public class MethDecl extends Decls {
         for (; formals != null; formals = formals.getNext()) {
             Id   paramId   = formals.getId();
             Type paramType = formals.getType().check(ctxt);
+            String methodName = "methodName";
             if (VarEnv.find(paramId.getName(), params) != null) {
                 ctxt.report(new Failure(paramId.getPos(),
                                         "Multiple uses of parameter " +
-                                        paramId));
+                                        paramId + ". " +
+                                        "First use here."));
+                ctxt.report(new Failure(paramId.getPos(),
+                                        "Multiple uses of parameter " +
+                                        paramId + ". " +
+                                        "Second use here."));
+                ctxt.report(new Failure("The two parameters to " +
+                                        methodName +
+                                        " have conflicting names."));
             }
             params = new VarEnv(paramId, paramType, params);
         }
