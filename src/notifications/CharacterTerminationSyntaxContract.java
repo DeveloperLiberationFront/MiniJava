9a18ec3e312ecd3238e25409ea3b58321a57834b
Jim Witschey <jim.witschey@gmail.com>
2014-11-14 17:36:41 -0500

    adds syntax errors

---
+++ src/notifications/CharacterTerminationSyntaxContract.java
@@ -0,0 +1,5 @@
+package notifications;
+
+public class CharacterTerminationSyntaxContract extends SyntaxContract {
+
+}
---
+++ src/notifications/UndeclaredSuperclassDiagnostic.java
@@ -0,0 +1,32 @@
+package notifications;
+
+import syntax.SuperAccess;
+import compiler.Declaration;
+import compiler.Position;
+import compiler.RichDiagnostic;
+
+public class UndeclaredSuperclassDiagnostic extends RichDiagnostic {
+
+	private Declaration subclassDeclaration;
+	private SuperAccess superAccess;
+
+	public UndeclaredSuperclassDiagnostic(SuperAccess superAccess,
+			Declaration subclassDeclaration) {
+		// instantiate "missing expression" and "scope accessibility" diagnostics
+		this.superAccess = superAccess;
+		this.subclassDeclaration = subclassDeclaration;
+	}
+
+	@Override
+	public String getText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Position getPos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
