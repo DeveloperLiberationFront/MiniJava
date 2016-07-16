9a59b98834152dd937b9dc5adc96129545d822a6
emerson <captain.emerson@gmail.com>
2016-07-15 17:11:02 -1000

    Fixes MiniJavaCompiler/MiniJavaLLVM/issues/#4

--- src/syntax/ClassType.java
+++ src/syntax/ClassType.java
@@ -132,7 +132,7 @@ public final class ClassType extends Type {
     /** Return the superclass, if any, of this class.
      */
     public ClassType getSuper() {
-        return extendsType.isClass();
+        return extendsType==null ? null : extendsType.isClass();
     }
 
     /** Test to see if this class is a supertype of another type.
